bc3431b2f5e085861b9ab5951237746a7d7ceef0
Kenneth Kehoe <kkehoe@ou.edu>
2019-11-02 21:50:50 -0600

    Adding some examples.

--- plotting/make_plot.py
+++ plotting/make_plot.py
@@ -1,21 +1,209 @@
-# importing the required module 
-import matplotlib.pyplot as plt 
-  
-# x axis values 
-x = [1,2,3] 
-# corresponding y axis values 
-y = [2,4,1] 
-  
-# plotting the points  
-plt.plot(x, y) 
-  
-# naming the x axis 
-plt.xlabel('x - axis') 
-# naming the y axis 
-plt.ylabel('y - axis') 
-  
-# giving a title to my graph 
-plt.title('My first graph!') 
-  
-# function to show the plot 
-plt.show() 
+#! /usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+
+# The simplest of simplest plots.
+if True:
+    # Here is the simplest call we can make. Things to notice.
+    # The axes are black, the line is blue. There are no symbols but
+    # a line is drawn connecting the 10 data points. The indeterminate
+    # axes is asumed to start at 0 and is the same number of values as
+    # deterministic axis. There is a bit of space added to both x and y
+    # axis so you can see the ends of the data. It is also making a guess
+    # as to how many axis labels to show. Also the size of the plot has
+    # a default. Any other things you notice?
+    plt.plot(np.arange(100))
+#    plt.plot(np.ones(10))
+    plt.show()
+
+
+# The simplest of plots.
+if False:
+    # x axis values
+    x = [1, 2, 3]
+    # corresponding y axis values
+    y = [2, 4, 1]
+
+    # plotting the points
+    plt.plot(x, y)
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+
+    # giving a title to my graph
+    plt.title('My first graph!')
+
+    # function to show the plot
+    plt.show()
+
+# Let's get a littl more complicated with plots.
+if False:
+    # numpy.linspace returns evenly spaced numbers over a specified interval.
+    # numpy.linspace(start, stop, num=50, endpoint=True, retstep=False,
+    #                dtype=None, axis=0)
+    x1 = np.linspace(0.0, 5.0)
+    x2 = np.linspace(0.0, 2.0)
+
+    # Transform the data using cosign and exponential functions
+    y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+    y2 = np.cos(2 * np.pi * x2)
+
+    # Create the plot figure and set up the space. We are defining two plots
+    # with one column and two rows. Set the first plot for all the
+    # following calls. Note the use of kwargs. This means all the rest of
+    # the possible keyword arguments that can be passed into the plottin call.
+    # You will not see all possible arguments in the function defintion because
+    # the the kwargs are passed to sub-routines as well. So you may need to
+    # dig a bit to get the argument you need.
+    # subplot(nrows, ncols, index, **kwargs)
+    plt.subplot(2, 1, 1)
+
+    # Plot the data using large plotting symbol and a line connecting
+    # the symbols. It will default to use black for axes and blue for
+    # line/symbol.
+    plt.plot(x1, y1, 'o-')
+
+    # Now we add a title to the plot.
+    plt.title('A tale of 2 subplots')
+
+    # And add a y-axis because we are scientists! But no x-lable, yet.
+    plt.ylabel('Damped oscillation')
+
+    # Now we switch to the second plot. So all following calls go to
+    # second plotting area.
+    plt.subplot(2, 1, 2)
+
+    # Make a plot using small symbols and dashed line. Also we choose to
+    # use green for plot
+    # symbol and line color.
+    plt.plot(x2, y2, '.--', color='green')
+
+    # Now we add a x-axis title.
+    plt.xlabel('time (s)')
+
+    # And of coures a y-axix because we are not heathens.
+    plt.ylabel('Undamped')
+
+    # So far everything has happened in plottin space but no plot was
+    # actually created. We need to no state the plot should be drawn.
+    # Up until now we could change any
+    # parameter and the plot would be "updated". Once we call to make the plot
+    # the latest settings will be used. By default a new window will appear.
+    plt.show()
+
+if False:
+    # Set the seed for random value. This means we get the same result
+    # every time.
+    np.random.seed(19680801)
+
+    # example data
+    mu = 100  # mean of distribution
+    sigma = 15  # standard deviation of distribution
+    x = mu + sigma * np.random.randn(437)
+
+    num_bins = 50
+
+    # This is setting up the figure and axes variables.
+    # Figure is the genear white space and ax is the plotting part. We can
+    # have one figure but as many axes as we want.
+    fig, ax = plt.subplots()
+
+    # the histogram of the data
+    n, bins, patches = ax.hist(x, num_bins, density=1)
+
+    # add a 'best fit' line
+    y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+         np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+    ax.plot(bins, y, '--')
+    ax.set_xlabel('Smarts')
+    ax.set_ylabel('Probability density')
+    # The 'r' at the start of a string means regular expression. This forces
+    # the string to be exactly as it is listed in memory.' The specail
+    # characters are used as escape sequences to set Greek letters.'
+    ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+    # Tweak spacing to prevent clipping of ylabel
+    fig.tight_layout()  # This adjusts the spaceing at ends of plot ranges.
+    plt.show()
+
+
+if False:
+    # this is to make some data to plot
+    np.random.seed(19680801)
+    # Make a 2-D array
+    data = np.random.randn(2, 100)
+
+    # this is setting up the figure and set ho many plots we are going to make.
+    # We are making two rows and two columns for a total of four plots.
+    # We are also stating how big the figure size should be. The numbers for
+    # figsize are *100 = pixels. So (5, 5) is (500, 500) pixels.
+    fig, axs = plt.subplots(2, 2, figsize=(5, 5))
+
+    # When we made the figure and returned axs, that is a list of length two
+    # with each element being a list of lenght two. This means we can make
+    # each plot or update each plot by referencing the axes. Start at upper
+    # left. [row, column] Axes is the plot, axis is the x and y axis.
+    print(axs)
+    axs[0, 0].hist(data[0])
+    axs[1, 0].scatter(data[0], data[1])
+    axs[0, 1].plot(data[0], data[1])
+    axs[1, 1].hist2d(data[0], data[1])
+
+    plt.show()
+
+if False:
+    # make these smaller to increase the resolution
+    dx, dy = 0.05, 0.05
+
+    # generate 2 2d grids for the x & y bounds. Don't worry much about this
+    # it is just to make some data to plot.
+    y, x = np.mgrid[slice(1, 5 + dy, dy),
+                    slice(1, 5 + dx, dx)]
+
+    z = np.sin(x)**10 + np.cos(10 + y*x) * np.cos(x)
+
+    # x and y are bounds, so z should be the value *inside* those bounds.
+    # Therefore, remove the last value from the z array.
+    z = z[:-1, :-1]
+    levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
+
+    # pick the desired colormap, sensible levels, and define a normalization
+    # instance which takes data values and translates those into levels.
+    # PiYG is the name of the colormap.
+    cmap = plt.get_cmap('PiYG')
+    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+
+    # Notice that the axes are split out using tuple syntax. This
+    # means we can reference with name only instead of needing to
+    # subset.
+    fig, (ax0, ax1) = plt.subplots(nrows=2)
+
+    # Now we make the plot for first row. We are using the color map
+    # defined above. If we didn't it would choose a default.
+    # Also notice that we are returnin a variable that is used to
+    # set the colorbar. This variael is referencing the plotted data
+    # specifically not including the x and y axes.
+    im = ax0.pcolormesh(x, y, z, cmap=cmap, norm=norm)
+    fig.colorbar(im, ax=ax0)
+    ax0.set_title('pcolormesh with levels')
+
+    # contours are *point* based plots, so convert our bound into point
+    # centers
+    # This is the second plot. Notice how we have called using ax1 from
+    # previous plt.subplots call.
+    cf = ax1.contourf(x[:-1, :-1] + dx/2.,
+                      y[:-1, :-1] + dy/2., z, levels=levels,
+                      cmap=cmap)
+    fig.colorbar(cf, ax=ax1)
+    ax1.set_title('contourf with levels')
+
+    # adjust spacing between subplots so `ax1` title and `ax0` tick labels
+    # don't overlap
+    fig.tight_layout()
+
+    plt.show()
