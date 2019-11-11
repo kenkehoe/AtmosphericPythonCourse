11d573e4d2b44a3662cc2d008e3fb149b7a33853
Kenneth Kehoe <kkehoe@ou.edu>
2019-11-11 15:23:59 -0600

    Adding some more examples and updates to better prints/comments.

--- scientific_libaries/data_xarray_2.py
+++ scientific_libaries/data_xarray_2.py
@@ -8,6 +8,11 @@ import numpy as np
 import xarray as xr
 import copy
 import pandas as pd
+# from pathlib import Path
+import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 if True:
 
@@ -17,19 +22,22 @@ if True:
     data2 = np.arange(0, num, dtype=float) * 3.
     # Make two additional arrays at half the dimentional size by stepping by 2.
     data3 = np.arange(0, num, 2, dtype=int)
-    data4 = np.arange(0, num, 2, dtype=float) * 4 + 40.44
+    data4 = (np.arange(0, num, 2, dtype=float) +
+             np.random.uniform(low=-20, high=20, size=data3.size))
 
-    # Create a datetime64 array to match the size of data we created.
+    # Create a datetime64 array with 1 minute time steps to match the size of data we created.
     time1 = np.array('2019-11-01T00:00:00', dtype='datetime64[m]') + np.arange(data1.size)
     time1 = time1.astype('datetime64[s]')
 
     # Make time array to match data3 size over the same range.
     time3 = np.array('2019-11-01T00:00:00', dtype='datetime64[m]') + np.arange(data3.size)
     # Add random seconds from 0 to 30 to each time value
-    time3 = time3.astype('datetime64[s]') + np.random.randint(0, 30, time3.size)
-    print(data1.size, data3.size)
-    print(time1.size, time3.size)
-    print()
+    time3 = time3.astype('datetime64[s]') + np.random.randint(-30, 30, time3.size)
+
+    if False:
+        print(data1.size, data3.size)
+        print(time1.size, time3.size)
+        print()
 
     # Create Dataset 1
     xr_ds1 = xr.Dataset(
@@ -41,7 +49,7 @@ if True:
         data_vars={'data3': ('time', data3), 'data4': ('time', data4)},
         coords={'time': ('time', time3)})
 
-    if True:
+    if False:
         # Using .loc will locate useing label based indexing.
         # Here we look for datetimes that fall within the range.
         xr_result = xr_ds1['time'].loc['2019-11-01T00:04:59':'2019-11-01T00:15:00']
@@ -97,6 +105,7 @@ if True:
         # First we use the inner join. Notice how few matches we find.
         result_merge = xr.merge((xr_ds1, xr_ds2), join='inner')
         print(result_merge.dims)
+
         # Here we use the outer join, so it will be all the unique times of the
         # combination of both Datasets. The length of the time dimenion is adjusted
         # to correctly match the results with locations where a time exiss but no data
@@ -113,6 +122,7 @@ if True:
         # method to use. But if we need to match the times this may not
         # be the correct method.
         print(result_merge)
+#        print(result_merge['time'])
 
     if False:
         # Here we use .align to get the two Datasets to match but not merge
@@ -130,15 +140,20 @@ if True:
         # don't need to have the time values matched this is a good
         # method to use. But if we need to match the times this may not
         # be the correct method.
-        print(result_align[0].data1)
-        print(result_align[1].data3)
+        print(result_align[0]['data1'])
+        print(result_align[1]['data3'])
+        if False:
+            print(result_align[0]['time'][0:10])
+            print(result_align[1]['time'][0:10])
+            print("result_align[0]['time'].size:", result_align[0]['time'].size)
+            print("result_align[1]['time'].size:", result_align[1]['time'].size)
 
         diff = result_align[0]['data1'].values - result_align[1]['data3'].values
         print(np.nanmean(diff))
 
 # If we use some of the functions of other libraries we can pull them into
 # Xarray to make it easier to initialize the Dataset.
-if True:
+if False:
 
     # Here we use pandas to make a time array with a 6 hour time step
     # for four years.
@@ -151,3 +166,33 @@ if True:
     # but pandas may be more intutive?
     np_time = np.array('2000-01-01', dtype='datetime64[h]') + np.arange(365 * 4) * 6
     ds_np_time = xr.Dataset({'data': ('time', np.arange(len(np_time))), 'time': np_time})
+
+    print("time == np_time:", (time == np_time).all())
+    print("\nds == ds_np_time:\n", (ds == ds_np_time).all())
+
+# Lest start changing the values to meet our needs.
+if False:
+
+    # First the orginal data size
+    print(xr_ds1['time'].size)
+
+    # Now let's calculate the mean for a day by grouping the data.
+    # This works for all time worded groups, hour, minute, year, month, ...
+    ds1_mean = xr_ds1.groupby('time.day').mean()
+    print('time.day mean:\n', ds1_mean)
+
+    # But what about a group that is not a standard group.
+    # Here we resample the data by an arbitray size and then make a of those groups.
+    ds1_mean = xr_ds1.resample(time='8min').reduce(np.mean)
+    print("time='8min\n': ", ds1_mean)
+
+if True:
+
+    # The bet way to understand what is going on is to plot the data.
+    xr_ds2['data3'].plot(label='data3')
+    xr_ds2['data4'].plot(label='data4')
+
+    ds2_mean = xr_ds2.resample(time='8min').reduce(np.mean)
+    ds2_mean['data4'].plot(label="ds2_mean.data4")
+    plt.legend()
+    plt.show()
