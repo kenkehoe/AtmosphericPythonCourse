1b97a4f98c861a6659a0faeea840e5cbdee93d75
Kenneth Kehoe <kkehoe@ou.edu>
2020-02-19 15:58:50 -0700

    some additional comments and print statements.

--- metadata/use_json.py
+++ metadata/use_json.py
@@ -2,14 +2,25 @@
 
 import json
 
-filename = 'example.json'
+filename = 'example.json'   # Filename of JSON file
+# If we needed to go to a different directory to get to the JSON
+# file we can use relative or full path to the file.
 with open(filename, "r") as read_file:
     docs = json.load(read_file)
 
+# The bes way to ust this part is to open the JSON file and see how it is
+# stuctured so you can see how the data types and nested data are preserved.
+# Feel free to mess with the JSON file to see how things change.
+# You can alwasy get it back to orginal state with a "git checkout example.json"
+print()
 print('type(docs):', type(docs))
 print()
+# Notice how docs is read in as a nested dictionary
 print('docs:\n', docs)
 print()
+
+# Here we can see how the JSON file preserves data type depending on the
+# syntax used in the JSON file.
 print("type(docs['a_variable_name'])", type(docs['a_variable_name']))
 print()
 print("type(docs['b_variable_name'])", type(docs['b_variable_name']))
@@ -25,3 +36,5 @@ print()
 print("docs['site']['DEN']['lon']:", docs['site']['DEN']['lon'])
 print()
 print("docs['d_list_name']:", docs['d_list_name'])
+print()
+print("docs['d_list_name']:", docs['d_list_name']["five"])
