852df8fe1dda1fec36d06a2b31e5a7ab4c083f6d
Kenneth Kehoe <kkehoe@ou.edu>
2021-03-09 10:19:29 -0700

    Updates

--- plotting/make_plot_real_data.py
+++ plotting/make_plot_real_data.py
@@ -214,7 +214,7 @@ if True:
 
     # Format the x-axis to show hour and minutes only.
     # ---- (1) ---- #
-    if True:
+    if False:
         myFmt = mdates.DateFormatter('%H:%M')
         axes.xaxis.set_major_formatter(myFmt)
 
