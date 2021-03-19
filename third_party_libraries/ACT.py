516bb4335547c9fd6e22863c8b5a5c2a4da1a5e6
Kenneth Kehoe <kkehoe@ou.edu>
2021-03-19 15:32:04 -0600

    Adding more examples

--- third_party_libraries/ACT.py
+++ third_party_libraries/ACT.py
@@ -16,17 +16,22 @@ if True:
     filename = str(Path('..', 'data', 'sgpmetE13.b1', 'sgpmetE13.b1.*.cdf'))
 
     if True:
-        # Using Xarray we can read one file using .open_dataset() method or if there are
+        # Using Xarray we can read one file using .open_dataset() method or if there is
         # more than one file to read the .open_mfdataset() method. Instead of needing to
         # know the details of xarray reading methods, we can just use the ACT method which
         # is a wrapper arround .open_mcdataset(). Notice how the filename uses a * character
-        # to allow us to read all files matching in the directory.
+        # to allow us to read all files matching in the directory. This is a feature of xarray
+        # open_mfdataset().
+
+        # Notice that this met_ds object is just an Xarray object with a few extra global
+        # attributes added. These are used for plotting lables and titles. All the Xarray
+        # methods will work on this object.
         met_ds = act.io.armfiles.read_netcdf(filename)
         print(met_ds)
 
     if False:
         # If the file does not exist xarray will through an error. But ACT can be told to
-        # not throw an error but return none instead.
+        # not throw an error, return None instead.
         filename2 = str(Path('..', 'data', 'sgpmetE13.b1', 'sgpmetE13.b1.20191101.cdf'))
         met_ds = act.io.armfiles.read_netcdf(filename2, return_None=True)
         print(met_ds)
@@ -51,6 +56,52 @@ if True:
                      'qc_logger_temp']
         met_ds = act.io.armfiles.read_netcdf(filename, drop_variables=drop_vars)
         print(met_ds)
+
+
+# Units are an under appreciated aspect of data. All netCDF files should use 'units' attribute
+# under each variable to correctly describe the data. Xarray does not have methods to update
+# units directly, but there is a library extension being developed to work with units. It has some issues
+# and does not preserve attribute order. To keep attributes more similar to CF ACT has its own
+# units method. It can also update units on a coordinate variable which is a litte more difficult
+# with Xarray due to the way coordinate values are modified.
+#
+# https://github.com/ARM-DOE/ACT/blob/master/examples/change_units.py
+#
+if False:
+    drop_vars = ['base_time', 'time_offset', 'qc_temp_mean', 'lat', 'lon', 'alt',
+                 'qc_rh_mean', 'vapor_pressure_mean',
+                 'qc_vapor_pressure_mean', 'vapor_pressure_std', 'wspd_arith_mean',
+                 'qc_wspd_arith_mean', 'wspd_vec_mean', 'qc_wspd_vec_mean', 'wdir_vec_mean',
+                 'qc_wdir_vec_mean', 'wdir_vec_std', 'tbrg_precip_total',
+                 'qc_tbrg_precip_total', 'tbrg_precip_total_corr',
+                 'qc_tbrg_precip_total_corr', 'org_precip_rate_mean',
+                 'qc_org_precip_rate_mean', 'pwd_err_code', 'pwd_mean_vis_1min',
+                 'qc_pwd_mean_vis_1min', 'pwd_mean_vis_10min', 'qc_pwd_mean_vis_10min',
+                 'pwd_pw_code_inst', 'qc_pwd_pw_code_inst', 'pwd_pw_code_15min',
+                 'qc_pwd_pw_code_15min', 'pwd_pw_code_1hr', 'qc_pwd_pw_code_1hr',
+                 'pwd_precip_rate_mean_1min', 'qc_pwd_precip_rate_mean_1min',
+                 'pwd_cumul_rain', 'qc_pwd_cumul_rain', 'pwd_cumul_snow',
+                 'qc_pwd_cumul_snow', 'logger_volt', 'qc_logger_volt',
+                 'qc_logger_temp', 'atmos_pressure', 'qc_atmos_pressure']
+    filename = Path('..', 'data', 'sgpmetE13.b1', 'sgpmetE13.b1.20191101.000000.cdf')
+    met_ds = act.io.armfiles.read_netcdf(str(filename), drop_variables=drop_vars)
+    print('\nInitial variables:')
+    for var_name in met_ds:
+        print('   ', var_name, met_ds[var_name].attrs['units'])
+
+    # A single variable can be updated with a method call and the variable name
+    var_name = 'temp_mean'
+    met_ds.utils.change_units(variables=var_name, desired_unit='degK')
+    print('\nUpdated variable:\n  ', var_name, met_ds[var_name].attrs['units'])
+
+    # Or all the variabls in the dataset can be updated if the units are compatible
+    # by ommiting the varible name. Notice how the relative humidity variables are not
+    # affected because the desired unit is a temperature which is not a compatible
+    # update.
+    met_ds.utils.change_units(desired_unit='degK')
+    print('\nAll units updated:')
+    for var_name in met_ds:
+        print('  ', var_name, met_ds[var_name].attrs['units'])
 
 
 # ARM uses an older format for embedded quality control. We can convert the embedded QC to follow
