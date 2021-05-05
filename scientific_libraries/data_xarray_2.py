d16e1ac773ceff5e1aeb964357f4b22bcd05b4bb
Kenneth Kehoe <kkehoe@ou.edu>
2021-05-05 09:59:48 -0600

    Minor updates

--- scientific_libraries/data_xarray_2.py
+++ scientific_libraries/data_xarray_2.py
@@ -15,7 +15,7 @@ register_matplotlib_converters()
 
 # Block 1
 if True:
-    # Create some data. data1 is int type and data2 is float type just to show
+    # Create some data. data1 is integer type and data2 is float type just to show
     # we can do that. data2 is also multiplied by 3.
     num = 100
     data1 = np.arange(0, num, dtype=int)
@@ -70,21 +70,22 @@ if True:
         # and change the values. We will use a time range to determine where to change
         # the data values to a new value.
         xr_da_data1.loc['2019-11-01T00:04:59':'2019-11-01T00:59:00'] = -9999
-        print((xr_da_data1))
+        print(xr_da_data1)
 
     if False:
         # Here we are going to match the times from Dataset 1 to Dataset 2.
-        # We use the select (.sel) method to indicate where we want to select values
+        # We use the select [.sel()] method to indicate where we want to select values
         # from Dataset 1 using the times from Dataset 2. It will use the nearest value
         # in time for matching. Notice the magic of how all the variables in the
-        # Dataset are selected.
+        # Dataset are selected. There are now 50 values.
         ds_result = xr_ds1.sel(time=xr_ds2['time'], method='nearest')
         print("\nds_result:\n", ds_result)
         print("\ntype(ds_result)\n:", type(ds_result))
 
     if False:
         # Or we can use .sel to perform a selection of a range
-        # across the full Dataset. Notice the magic of how all the variables in the
+        # across the full Dataset with the slice() function.
+        # Notice the magic of how all the variables in the
         # Dataset are selected. The time dimension is reduced to size 11 from 100.
         ds_result = xr_ds1.sel(time=slice('2019-11-01T00:04:59', '2019-11-01T00:15:00'))
         print("\nds_result:\n", ds_result)
@@ -93,12 +94,14 @@ if True:
         # But what if we have a large gap and the nearest value falls outside
         # a tolerable range. We can use .reindex method with a toleracne to indicate
         # which values should be matched. The values that don't have a match are
-        # set to NaN. We need to use timedelta64 to set the tolerance value which
+        # set to NaN. We need to use timedelta64() to set the tolerance value which
         # includes a time unit. Remember we added a random 0 - 30 seconds to the
         # time values. Notice the magic of how all the variables in the
-        # Dataset are updated.
+        # Dataset are updated. The size of the dataset is still 100 samples, just
+        # where a match could not be found it inserts NaN for data. Time step
+        # is still in the time coordinate.
         ds_result = xr_ds2.reindex(time=xr_ds1['time'], method='nearest',
-                                tolerance=np.timedelta64(20, 's'))
+                                   tolerance=np.timedelta64(20, 's'))
 
         # If no values match within tollerence it will still make
         # a new Dataset with all the time steps, but the data will all be set to
@@ -112,7 +115,8 @@ if True:
         # both Datasets it will cause an error.
         # First we use the inner join. Notice how few (if any) matches we find
         # because of the random times. Times need to match exactly.
-        result_merge = xr.merge((xr_ds1, xr_ds2), join='inner')
+        if True:
+            result_merge = xr.merge((xr_ds1, xr_ds2), join='inner')
 
         # Here we use the outer join, so it will be all the unique times of the
         # combination of both Datasets. The length of the time dimenion is adjusted
@@ -120,7 +124,8 @@ if True:
         # exists set to NaN. There is no tolerance. If they are slightly off
         # a new value is added. This is why this returned Dataset is larger
         # than either of the input Datasets.
-#        result_merge = xr.merge((xr_ds1, xr_ds2), join='outer')
+        if False:
+            result_merge = xr.merge((xr_ds1, xr_ds2), join='outer')
 
         # We need to understand what is being matched. It is not matching
         # times. It is matching indexes and expanding the dimension sizes
@@ -133,15 +138,16 @@ if True:
 #        print(result_merge['time'])
 
     if False:
-        # Here we use .align to get the two Datasets to match but not merge
+        # Here we use .align() to get the two Datasets to match but not merge
         # them into the same Dataset. The returned value is a tuple of two
         # new Datasets.
         result_align = xr.align(xr_ds1, xr_ds2, join='inner')
-        print(result_align[0].dims, result_align[1].dims)
-
         result_align = xr.align(xr_ds1, xr_ds2, join='outer')
-        print(result_align[0].dims, result_align[1].dims)
-        print()
+
+        if True:
+            print('inner:\n', result_align[0].dims, result_align[1].dims)
+            print('outer:\n', result_align[0].dims, result_align[1].dims)
+            print()
 
         # We need to understand what is being matched. It is not matching
         # times. It is matching indexes and expanding the dimension sizes
@@ -159,8 +165,9 @@ if True:
             print("result_align[0]['time'].size:", result_align[0]['time'].size)
             print("result_align[1]['time'].size:", result_align[1]['time'].size)
 
-#        diff = result_align[0]['data1'].values - result_align[1]['data3'].values
-#        print("\nnp.nanmean(diff):", np.nanmean(diff))
+        if False:
+            diff = result_align[0]['data1'].values - result_align[1]['data3'].values
+            print("\nnp.nanmean(diff):", np.nanmean(diff))
 
 # Block 2
 # If we use some of the functions of other libraries we can pull them into
@@ -174,7 +181,7 @@ if False:
     # We will create a new Xarray Dataset with a range of numbers matching
     # the number of time samples we created.
     # The pandas time is used to initialize the xarray Dataset. Because
-    # pandas and xarray play well together it jus works.
+    # pandas and xarray play well together it just works.
     ds_pd_time = xr.Dataset({'data': ('time', np.arange(len(pd_time))), 'time': pd_time})
 
     # To create this same time step with numpy we can do this. Both get the same result
@@ -182,13 +189,14 @@ if False:
     np_time = np.array('2000-01-01', dtype='datetime64[h]') + np.arange(365 * 4) * 6
     ds_np_time = xr.Dataset({'data': ('time', np.arange(len(np_time))), 'time': np_time})
 
+    # Print if the all the time steps created by Pandas and Numpy are the same
     print("\npd_time == np_time:", (pd_time == np_time).all())
     print("\nds_pd_time == ds_np_time:\n", (ds_pd_time == ds_np_time).all())
     print()
 
 # Block 3
-# Lest start changing the values to meet our needs. Need to change first block to True
-# for us to use that data. 
+# Lest start changing the values to meet our needs.
+# !!!! Need to change first block to True for us to use that data !!!!
 if False:
 
     # First the orginal data size
@@ -206,7 +214,7 @@ if False:
 
 # Block 4
 if False:
-    # The bet way to understand what is going on is to plot the data.
+    # The best way to understand what is going on is to plot the data.
 
     # Using methods attached to the Xarray Dataset we can call a matplotlib
     # function to plot the data. We will also add a lable to use in next step
@@ -227,5 +235,8 @@ if False:
     # To lable the different lines we add a legend.
     plt.legend()
     # Now we tell matplotlib to draw the plot. Up until this point
-    # no plot was actually created.
+    # no plot was actually created. Notice how the xaxis is already formatted
+    # nicely for time. Xarray knows the axis is time and makes a guess as to
+    # the correct formatting. Since data4 does not have a long_name attribute
+    # it uses the variable name in the Xarray Dataset.
     plt.show()
