02235206b8dd607cf583607f0d093bab6e213077
Ken Kehoe <kkehoe@ou.edu>
2023-10-02 17:39:04 -0600

    Adding information on setting PYTHONPATH

--- workflow/repo_example.py
+++ workflow/repo_example.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python3
 
-from mypythonlib.myfunctions import haversine
+# Option 1. Copy and past this to the command line.
+# export PYTHONPATH="$PYTHONPATH:$PWD/repository_example"
+
+# Option 2. Uncomment these lines
+# import sys
+# from pathlib import Path
+
+# path_to_library = Path(Path.cwd(), 'repository_example')
+# sys.path.append(path_to_library)
+
+from repository_example.mypythonlib.myfunctions import haversine
 
 result = haversine(4.895168, 52.370216, 13.404954, 52.520008)
 print(f"result = {result}")
